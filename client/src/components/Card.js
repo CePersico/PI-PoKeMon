af3ff9d3dfa92db1e8c16565797b8636ab777704
CePersico <cepersico.3@gmail.com>
2021-10-10 18:39:34 -0300

    algo de css

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -3,9 +3,9 @@ import style from './Card.module.css';
 
 export default function Card({name, sprite, attack, types}) {  // paso por props, por eso no necesito trearme ningun estado 
     return (
-        <div >
+        <div className={style.efectolupa } >
             <h2 className = {style.name}>{name}</h2>
-            <img src = {sprite} alt = 'Not found' width = "200px" height = "200px"/>
+            <img className={style.efectolupa }src = {sprite} alt = 'Not found' width = "200px" height = "200px"/>
             <h6>{attack}</h6>
             <h6>{types}</h6>            
         </div>
